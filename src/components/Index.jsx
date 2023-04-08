192369967c68d54a3f77028bb51c8c580fbdc518
Saad Ahmed <saadahmed7262@gmail.com>
2023-04-08 15:05:04 +0500

    some changes

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -3,11 +3,11 @@ import Header from '../core/header';
 import Content from './Content';
 import Footer from '../shared/footer';
 
-const index = () => {
+const Index = () => {
     return (<React.Fragment>
         <Header />
         <Content />
     </React.Fragment>)
 }
 
-export default index;
+export default Index;
