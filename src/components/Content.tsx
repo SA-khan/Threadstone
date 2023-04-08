9236252b078f7045fcff92b047b4729e84a8bbe1
Saad Ahmed <saadahmed7262@gmail.com>
2023-04-08 14:16:33 +0500

    tailwind integrated

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -8,6 +8,15 @@ const Content = () => {
         <div className='text-center'>
             <p className='bg-light p-4 m-1 display-5'>We Gather to Bring Change</p>
         </div>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 float-right m-2">
+  <div className="shrink-0">
+    <img className="h-12 w-12" src="/images/logo.png" alt="ChitChat Logo" />
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black">ChitChat</div>
+    <p className="text-slate-500">You have a new message!</p>
+  </div>
+</div>
     </React.Fragment>)
 }
 
